43e5fe3f55638e72fb38b98f6d867dfb84bc6932
Julian <julian.franco@icloud.com>
2023-04-11 21:34:36 -0400

    refactor routing in skils.js

---
+++ data/skill-data.js
@@ -0,0 +1,11 @@
+const skills = [
+  {text: 'Mix records', learned: true, _id: 125223},
+  {text: 'Open and repair Macs', learned: true, _id: 127904},
+  {text: 'Code in JavaScript', learned: true, _id: 139608},
+  {text: 'Code in Python', learned: false, _id: 139658},
+  {text: 'Code in React', learned: false, _id: 139708},
+]
+
+export { 
+	skills
+}
---
+++ routes/skills.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { skills } from '../data/skill-data.js'
+
+const router = Router()
+
+// GET localhost:3000/skills
+router.get('/', function(req, res) {
+  res.render('skills/index', {
+    skills: skills
+  })
+})
+
+export { router }
