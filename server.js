81649e478c86bd86f2c5a97479b26e156d22faf1
Julian <julian.franco@icloud.com>
2023-04-12 19:33:17 -0400

    add models and schema

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import createError from 'http-errors'
 import logger from 'morgan'
-import { skills } from './data/skill-data.js'
+
+// connect to the database with Mongoose
+import './config/database.js'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
