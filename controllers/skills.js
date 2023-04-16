6f3301bedb9eaa03770dfcc402f7918450943129
Julian <julian.franco@icloud.com>
2023-04-15 21:57:11 -0400

    add button to index page

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,6 +1,7 @@
 // Import the model that we exported in the Todo.js model file
 import { Skill } from '../models/skill.js'
 // Inside the index controller, use the Skill model to query for ALL skills
+
 function index(req, res) {
   Skill.find({})
   .then(skills => {
